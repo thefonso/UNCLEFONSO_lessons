e5c998625a7e2643ee180f3603567bdc32d44ba1
thefonso <thefonso@thefonso.com>
2017-04-07 18:27:26 -0700

    addedcli cheatsheet

--- 00-week/ruby_basics/00_strings/00-local-variables.rb
+++ 00-week/ruby_basics/00_strings/00-local-variables.rb
@@ -1,11 +1,14 @@
-# When Ruby wants to store an object it finds available memory in an area called the heap, it then puts the object's data there.
-# But how will we know where we put our object? We need something to keep track of its address.
-# 
+# STRINGS: local variables
+#
+# When Ruby wants to store a value, it finds available memory in an area called the heap, 
+# it then puts the value there.
+# But how will you know where you put your value? You need something to keep track of its address.
+# We can use variables for this.
 # Variables are like little fingers that point to memory addresses! 
-# They let us keep track of where our objects are -- so you can access objects through variables.
+# They let you keep track of where our values are -- so you can access values through variables.
 
 
-five = 5      # five is a variable, 5 is the object it is pointing to
+five = 5      # five is a variable, 5 is the value it is pointing to
 
 
 # variables can be set to point to new values
@@ -16,7 +19,7 @@ five            # => "five"
 
 
 # In Ruby we name long variables with snake_case
-this_is_an_example_of_snake_case = "it uses underscores where the spaces go"
+this_is_an_example_of_snake_case = "it uses underscores where the spaces go" # !> assigned but unused variable - this_is_an_example_of_snake_case
 
 # Constants are definitions that can be recalled later. They can't be changed once defined
 # Define them with all upper case
@@ -29,15 +32,20 @@ ANSWER_TO_EVERYTHING = 42
 
 # TERMS
 #
-# heap: A very simple explanation is that the heap is the portion of memory where dynamically allocated memory resides
-# Memory allocated from the heap will remain allocated until one of the following occurs:
-
-# 1. The memory is free'd
-# 2. The program terminates
-
-# Contrast this with -stack- memory which is where local variables (those defined within a method) live. 
-# Memory allocated on the stack generally only lives until the function returns 
-# (there are some exceptions to this, e.g. static local variables).
+# Stack:
+#
+# When a function or a method calls another function which in turns calls another function etc., the execution of all those functions remains suspended until the very last function returns its value.
+#
+# This chain of suspended function calls is the stack, because elements in the stack (function calls) depend on each other.
+#
+# The stack is important to consider in exception handling and thread executions.
+#
+# Heap:
+#
+# The heap is simply the memory used by programs to store variables. Element of the heap (variables) have no dependencies with each other and can always be accessed randomly at any time.
+#
+# I like the accepted answer better since it's even more low level.
 
 # you can see the memory location with the ruby .object_id method
-five.object_id # => 70173340725520
+five.object_id # => 70101932257160
+
